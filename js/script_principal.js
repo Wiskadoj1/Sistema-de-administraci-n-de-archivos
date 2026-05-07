2c4b654a9aae3a47e774aaa321f477e052571a56
braulio <brageant@gmail.com>
2026-05-07 16:48:59 -0600

    fx: Se arreglo el problema de rutas

--- js/script_principal.js
+++ js/script_principal.js
@@ -2,6 +2,6 @@ let usuario = localStorage.getItem("usuario") || "";
 document.getElementById("bienvenida").innerHTML = "Bienvenido al sistema " + usuario;
 function cerrarSesion(){
     localStorage.removeItem("usuario");
-    window.location.href = "/index.html";
+    window.location.href = "../index.html";
 }
 document.getElementById("btnCerrarSesion").addEventListener("click", cerrarSesion)
